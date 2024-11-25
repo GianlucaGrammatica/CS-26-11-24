265cab39dbcf241e6fe7cbc5b0b3fa607e1e5ea7
Abcia2 <105174769+Abcia2@users.noreply.github.com>
2024-11-25 17:33:02 +0100

    Aggiunta, rimozione, ricerca (circa) help

---
+++ CS-26-11-24/InventarioProdotti.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_26_11_24
+{
+    internal class InventarioProdotti
+    {
+        Dictionary<int, (string, int)> Inventario = new Dictionary<int, (string, int)>();
+
+        public void AggiungiProdotto(int ID, string Nome, int Quantity)
+        {
+            if (Inventario.ContainsKey(ID))
+            {
+                Inventario[ID] = (Nome, Quantity);
+            }
+            else
+            {
+                Inventario.Add(ID, (Nome, Quantity));
+            }
+        }
+
+        public void AggiornaProdotto(int ID, string newNome, int newQuantity)
+        {
+            if (!Inventario.ContainsKey(ID))
+            {
+                throw new KeyNotFoundException("Prodotto non trovato.");
+            }
+
+            Inventario[ID] = (newNome, newQuantity);
+        }
+
+        public void RimuoviProdotto(int ID)
+        {
+            if (!Inventario.ContainsKey(ID))
+            {
+                throw new KeyNotFoundException("Prodotto non trovato.");
+            }
+
+            Inventario.Remove(ID);
+        }
+
+        public string VisualizzaInventario()
+        {
+            string inventarioStringa = "";
+
+            foreach (var prodotto in Inventario)
+            {
+                inventarioStringa += $"ID: {prodotto.Key}, Nome: {prodotto.Value.Item1}, Quantità: {prodotto.Value.Item2}\n";
+            }
+
+            return inventarioStringa;
+        }
+
+        public string RicercaPerNome(string nome)
+        {
+            string risultati = "";
+
+            foreach (var prodotto in Inventario)
+            {
+                if (prodotto.Value.Item1 == nome)
+                {
+                    risultati += $"ID: {prodotto.Key}, Nome: {prodotto.Value.Item1}, Quantità: {prodotto.Value.Item2}\n";
+                }
+            }
+
+            return risultati;
+        }
+    }
+}
